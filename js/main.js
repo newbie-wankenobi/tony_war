5fab9c291c0956b30c88b1f0d667de3431a92c62
Edward Estese <tonyestese@hotmail.com>
2016-02-03 16:15:58 -0800

    Ezra session 2

--- js/main.js
+++ js/main.js
@@ -5,37 +5,31 @@ var currentTurn;
 
 // Player constructor function
 var Player = function(id) {
-  this.name = "Player" + (id + 1);
+  this.name = "Player " + (id + 1);
   this.id   = "p" + id;
+  this.hand = [];
 }
 
 // List of Players
-var players = [
-                new Player(0),
-                new Player(1)
-              ];
+var players = [new Player(0), new Player(1)];
 
 // Model the Board
 var board = [null, null];
 
-$('.switch').click(function() {
-  nextTurn();
-  printPlayers();
-  render();
-})
-
 // Shuffle the deck
 
-// Shuffle sound everytime
-
 function shuffleArray(array) {
-  for  (var i = array.length - 1; i > 0; i--));
+  for (var i = array.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i +1));
       var temp = array[i];
       array[i] = array [j];
       array[j] = temp;
+  }
+  console.log(i);
+  return array;
 }
-return array;
+
+// Shuffle sound everytime
 
 // Cards dealt to each player 26 a piece
 
@@ -43,61 +37,47 @@ $('#deal').click(function() {
     dealCard(randomCard());
 })
 
-var cardsInDeck = new Array();
-var numberOfCardsInDeck = 52;
-cardsInDeck[0] = "DiamondsAce";
-cardsInDeck[1] = "DiamondsKing";
-cardsInDeck[2] = "DiamondsQueen";
-cardsInDeck[3] = "DiamondsJack";
-cardsInDeck[4] = "Diamonds10";
-cardsInDeck[5] = "Diamonds9";
-cardsInDeck[6] = "Diamonds8";
-cardsInDeck[7] = "Diamonds7";
-cardsInDeck[8] = "Diamonds6";
-cardsInDeck[9] = "Diamonds5";
-cardsInDeck[10] = "Diamonds4";
-cardsInDeck[11] = "Diamonds3";
-cardsInDeck[12] = "Diamonds2";
-cardsInDeck[13] = "SpadesAce";
-cardsInDeck[14] = "SpadesKing";
-cardsInDeck[15] = "SpadesQueen";
-cardsInDeck[16] = "SpadesJack";
-cardsInDeck[17] = "Spades10";
-cardsInDeck[18] = "Spades9";
-cardsInDeck[19] = "Spades8";
-cardsInDeck[20] = "Spades7";
-cardsInDeck[21] = "Spades6";
-cardsInDeck[22] = "Spades5";
-cardsInDeck[23] = "Spades4";
-cardsInDeck[24] = "Spades3";
-cardsInDeck[25] = "Spades2";
-cardsInDeck[26] = "HeartsAce";
-cardsInDeck[27] = "HeartsKing";
-cardsInDeck[28] = "HeartsQueen";
-cardsInDeck[29] = "HeartsJack";
-cardsInDeck[30] = "Hearts10";
-cardsInDeck[31] = "Hearts9";
-cardsInDeck[32] = "Hearts8";
-cardsInDeck[33] = "Hearts7";
-cardsInDeck[34] = "Hearts6";
-cardsInDeck[35] = "Hearts5";
-cardsInDeck[36] = "Hearts4";
-cardsInDeck[37] = "Hearts3";
-cardsInDeck[38] = "Hearts2";
-cardsInDeck[39] = "ClubsAce";
-cardsInDeck[40] = "ClubsKing";
-cardsInDeck[41] = "ClubsQueen";
-cardsInDeck[42] = "ClubsJack";
-cardsInDeck[43] = "Clubs10";
-cardsInDeck[44] = "Clubs9";
-cardsInDeck[45] = "Clubs8";
-cardsInDeck[46] = "Clubs7";
-cardsInDeck[47] = "Clubs6";
-cardsInDeck[48] = "Clubs5";
-cardsInDeck[49] = "Clubs4";
-cardsInDeck[50] = "Clubs3";
-cardsInDeck[51] = "Clubs2";
+var deck = [
+  'd02', 'd03', 'd04', 'd05', 'd06', 'd07', 'd08', 'd09', 'd10', 'dJ', 'dQ', 'dK', 'dA',
+  'c02', 'c03', 'c04', 'c05', 'c06', 'c07', 'c08', 'c09', 'c10', 'cJ', 'cQ', 'cK', 'cA',
+  's02', 's03', 's04', 's05', 's06', 's07', 's08', 's09', 's10', 'sJ', 'sQ', 'sK', 'sA',
+  'h02', 'h03', 'h04', 'h05', 'h06', 'h07', 'h08', 'h09', 'h10', 'hJ', 'hQ', 'hK', 'hA'
+];
 
+
+
+
+function dealCards(cards) {
+  shuffleArray(cards);
+  for (var i = 0; i < cards.length; i++) {
+    if (i % 2 === 0) {
+      players[0].hand.push(cards[i]);
+    } else {
+      players[1].hand.push(cards[i]);
+    }
+  }
+}
+  // players[0]
+
+//   if (numberOfCardsInDeck == 0) return
+// false;
+/*  var img = document.createElement("img");
+    img.src =
+    document.body.appendChild(img);
+    removeCard(i); */
+
+// dealCard(deck)
+
+function randomCard() {
+  return Math.floor(Math.random() * numberOfCardsInDeck);
+}
+
+function removeCard(c) {
+  for (j=c; j <= numberOfCardsInDeck - 2; j++) {
+      cardsInDeck[j] = cardsInDeck[j+1];
+  }
+  numberOfCardsInDeck --;
+}
 // Place drawn cards on board in center
 
 // Post score in statistics box real-time
